d1b29a864bb91531665a08c00e7fbba049b3f3d9
rbjunk <118035460+rbjunk@users.noreply.github.com>
2025-11-29 13:32:35 -0500

    Update the tree view to allow selection of files and set the active file to the selected file. Also add comments explaining other functions.

--- gui.py
+++ gui.py
@@ -1,8 +1,10 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QTextEdit, QTreeWidget, QDockWidget, QTreeView, QInputDialog
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QTextEdit, QTreeWidget, QDockWidget, QTreeView, QInputDialog, QLineEdit
 from PyQt6.QtGui import QIcon, QAction, QFileSystemModel
 from PyQt6.QtCore import Qt, pyqtSignal
 from pathlib import Path
 from file_actions import *
+
+#Mixin class to center any window on the screen
 class CenterMixin():
     def center(self):
         qr = self.frameGeometry()
@@ -10,6 +12,7 @@ class CenterMixin():
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+#Default window class
 class defaultWindow(QWidget, CenterMixin):
     def __init__(self):
         super().__init__()
@@ -24,26 +27,27 @@ class defaultWindow(QWidget, CenterMixin):
         self.setWindowTitle("File App")
         self.show()
 
+#Main window of the program that houses all main actions
 class mainWindow(QMainWindow, CenterMixin):
     def __init__(self):
         super().__init__()
-        self.current_directory = str(Path.cwd())
+        self.current_directory = str(Path.cwd()) #variable to hold the current selected directory, initialized to the top of the current working directory
+        self.current_file = None #variable to hold the current selected directory, initialized to None
         self.initUI()
 
     def initUI(self):
-
-
     #Actions for menu bar options
+    #Menubar action exit program
         exitAct = QAction('Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(self.close)
-
+    #Menubar action create new file
         newFileAct = QAction("New File", self)
         newFileAct.setShortcut("Ctrl+N")
         newFileAct.setStatusTip("Create a new file")
         newFileAct.triggered.connect(self.createFileAction)
-    #Create menu bar
+    #Create menu bar and add menubar members
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(newFileAct)
@@ -52,12 +56,15 @@ class mainWindow(QMainWindow, CenterMixin):
     #Central text editor widget
         textEdit = QTextEdit()
         self.setCentralWidget(textEdit)
+
     #File tree dock widget
         self.file_tree_widget = fileTreeView()
-        self.file_tree_widget.directorySelected.connect(self.onDirectorySelected)
+        self.file_tree_widget.directorySelected.connect(self.onDirectorySelected) #when a directory change is detected, call the OnDirectorySelected method with the appropriate directory path
+        self.file_tree_widget.fileSelected.connect(self.onFileSelected) #when a file change is detected, call the OnFileSelected method with the appropriate file path
         dock = QDockWidget("File Tree", self)
         dock.setWidget(self.file_tree_widget)
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
+
     #Status bar at the bottom of the page
         self.statusBar()
 
@@ -68,36 +75,48 @@ class mainWindow(QMainWindow, CenterMixin):
         self.show()
 
     def onDirectorySelected(self, directory_path):
+        #Update the current working directory when a user selects a directory
         self.current_directory = directory_path
         self.statusBar().showMessage(f"Current directory: {self.current_directory}")
+
+    def onFileSelected(self, file_path):
+        #update the current file when selected when a user selects a file
+        self.current_file = file_path
+        self.statusBar().showMessage(f"Current file: {self.current_file}")
+
     def createFileAction(self):
-        file_name, ok = QInputDialog.getText(self, "New File", "Enter file name:")
+        file_name, ok = QInputDialog.getText(self, "New File", "Enter file name:", QLineEdit.EchoMode.Normal, "File.txt")
         if ok and file_name:
             new_file_path = self.current_directory+ "/" + file_name
             createNewFile(new_file_path)
 
 class fileTreeView(QTreeView):
-    directorySelected = pyqtSignal(str)
-
+    directorySelected = pyqtSignal(str) #custom signal to emit the current selected directory
+    fileSelected = pyqtSignal(str) #custom signal to emit the current selected file and parent directory
     def __init__(self):
         super().__init__()
         self.file_system = QFileSystemModel()
+        #initialize the root file path in the tree as the top of the current working directory
         root_file_path = str(Path.cwd())
         self.file_system.setRootPath(root_file_path)
         self.setModel(self.file_system)
 
         root_index = self.file_system.index(root_file_path)
         self.setRootIndex(root_index)
-
+        #connect the onItemClicked method to the action of clicking on an item in the tree, the clicked signal will send an index indicating which item was clicked
         self.clicked.connect(self.onItemClicked)
 
     def onItemClicked(self, index):
-        file_path = self.file_system.filePath(index)
+        #handle when an item in the tree is selected
+        path = self.file_system.filePath(index) #set the file path to the current path at index
+        #if the selected item index points to a directory, then emit the directory
         if self.file_system.isDir(index):
-            self.directorySelected.emit(file_path)
+            self.directorySelected.emit(path)
         else:
-            parent_path = str(Path(file_path).parent)
+        #if the selected item index points to a file, then emit the parent directory of the file and then emit the file path
+            parent_path = str(Path(path).parent)
             self.directorySelected.emit(parent_path)
+            self.fileSelected.emit(path)
 
         
     
